f8007dc698e610f380b5031f1aab87369e9f701a
NANDYALANAGIREDDY <sivanagireddynandyala999@gmail.com>
2024-12-05 21:59:46 +0530

    scg

---
+++ ec2-with-SCG/scg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow inbound traffic on ports 22 and 80"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows SSH from anywhere, consider limiting this
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allows HTTP from anywhere
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allows all outbound traffic
+  }
+}
