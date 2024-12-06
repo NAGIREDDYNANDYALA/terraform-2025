023931231cdff4bc7cfd951b4218a7baf5984583
NAGIREDDYNANDYALA <118894739+NAGIREDDYNANDYALA@users.noreply.github.com>
2024-12-06 21:11:08 +0530

    Update ec2.tf

--- ec2-with-SCG/ec2.tf
+++ ec2-with-SCG/ec2.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "terraform-ec2" {
-  ami = "ami-0453ec754f44f9a4a"
-  instance_type = "t2.micro"
-  key_name = myself.key
-  security_groups = web_sg
+resource "aws_instance" "Launched-by-IAC" {
+  ami                    = "ami-0453ec754f44f9a4a"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.my-key-pair.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  depends_on             = [aws_security_group.web_sg]
   tags = {
-    Env = "Dev"
+    "Env" = "Dev"
   }
 }
